1b2dae4f4b4a8d5b9b3e78fc22339d1443b678ca
türkay ürkmez <turkay.urkmez@outlook.com>
2024-10-31 11:10:24 +0300

    project tracker app initialized

---
+++ projectTracker/src/app/models/mocks/depaerments.mock.ts
@@ -0,0 +1,7 @@
+import { Department } from "../department.model";
+
+export const fakeDepartments: Department[] = [
+    { id: 1, name:'Departman A'},
+    { id: 2, name:'Departman B'},
+    { id: 3, name:'Departman C'},
+]
